c1ebea50eb58eddcd1fb8794920c7b5a3bad335c
herbheebhart <abibatsalaudeen71@gmail.com>
2024-04-17 14:31:12 +0100

    final assement

--- lib/Pages/weather_page.dart
+++ lib/Pages/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:weather_app/models/Weather_model.dart';
 import 'package:weather_app/Service/weather_service.dart';
 
@@ -29,6 +30,29 @@ class _WeatherPageState extends State<WeatherPage> {
       print(e);
     }
   }
+  // weather  
+  String getWeatherAnimation(String? mainCondition){
+    if(mainCondition == null) return 'assets/sunny.json';
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloudy.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rainny.json';
+       case 'thunderstorm':
+        return 'assets/main.json'; 
+        case 'clear':
+          return 'assets/sunny.json';
+          default:
+            return 'assets/sunny.json';
+    }    
+  }
   @override
   void initState(){
     super.initState();
@@ -40,13 +64,44 @@ class _WeatherPageState extends State<WeatherPage> {
     // we use scaffold to give our weather a structure
     return Scaffold (
       // Every items are centered in the screen
+      backgroundColor: Colors.grey[800],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           // the city and the weather temperature is show using text widgets and if we dont have the weathr yet it show us loading weather.
           children: [
-            Text(_weather?.cityName ?? "Loading weather....."),
-            Text('${_weather?.temperature.round()}0C')
+            Text(_weather?.cityName ?? "Loading weather.....",
+            // ignore: prefer_const_constructors
+            style: TextStyle(
+              fontSize:24,
+              fontWeight:FontWeight.bold,
+              color:Colors.white,
+            ),
+            ),
+            // animation
+            SizedBox(height: 20),
+           Lottie.asset(getWeatherAnimation(_weather?.mainCondition )),
+              // Temperature
+            Text('${_weather?.temperature.round()}\u{00B0}C',
+            // ignore: prefer_const_constructors
+            style:TextStyle(
+              fontSize:24,
+              fontWeight: FontWeight.bold,
+              color:Colors.red,
+
+            ),
+            ),
+            // weather condition
+               Text(_weather?.mainCondition ?? "",
+                 // ignore: prefer_const_constructors
+                 style:TextStyle(
+              fontSize:32,
+              fontWeight: FontWeight.bold,
+              color:Color.fromARGB(255, 15, 226, 139),
+                 ),
+               ),
+
+          
           ],
         )
       )
